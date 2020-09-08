beeb4f415c2e7d884a3dcfcae1f59aaba40fb813
S1810307008 (Eder Michael) <s1810307008@students.fh-hagenberg.at>
2020-09-08 21:44:25 +0200

    Added cart-button for team-checkout

---
+++ 06_webcomponents/team-checkout/fragments/cart-button.js
@@ -0,0 +1,64 @@
+class CartButton extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({mode: "open"});
+  }
+
+  get productId() {
+    return this.getAttribute('productId');
+  }
+
+  set productId(value) {
+    this.setAttribute('productId', value);
+  }
+
+  get cartStyle() {
+    return `<style>
+
+      button {
+      color: white;
+      background: #29c296;
+      border-radius: .15em;
+      padding:  0.55em;
+      border: none;
+      box-shadow: 7px 6px 14px -8px rgba(0,0,0,0.75);
+    }
+
+      button:hover {
+      cursor: pointer;
+    }
+
+      button:focus {
+      outline :#0062cc;
+    }
+      button:active {
+      background: #39d2ad;
+    }
+
+
+    </style>`;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+                         ${this.cartStyle}
+                          <button class="button--primary">
+                             <strong><i class="fas fa-shopping-cart"></i>In den Einkaufswagen</strong>
+                          </button>`
+  }
+
+  disconnectedCallback() {
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+
+  }
+
+}
+
+window.customElements.define('me-cart-button', CartButton);
