29ed8df15c4237d65a19774c810c0201741a45d7
Michael Eder <Michael.eder@liwest.at>
2020-09-17 09:42:34 +0200

    Added box shadow for Dashlet containers

--- 07_appshell/src/app-1/vue/src/components/Dashlet.vue
+++ 07_appshell/src/app-1/vue/src/components/Dashlet.vue
@@ -29,6 +29,8 @@ export default {
   max-height: 12.5em;
   flex-wrap: wrap;
   padding: 2em;
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 1px 3px 0 rgba(0, 0, 0, 0.12);
 }
 
 .row {
